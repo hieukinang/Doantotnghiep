0b7243a12d0e25bac88e1a543ea92a8c5ce3af2d
tiencong261 <tiencong261@gmail.com>
2025-10-10 18:30:52 +0700

    update ui store shipper management

---
+++ Admin/src/page/StoreProfileDetail.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const StoreProfileDetail = () => {
+  return (
+    <div className='py-4 space-y-6'>
+      <h1> Xin chaof </h1>
+    </div>
+  )
+}
+
+export default StoreProfileDetail;
