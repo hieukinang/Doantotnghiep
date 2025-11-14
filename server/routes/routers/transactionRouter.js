3dd611c444839349ef6d4d1bfccee6f253904b07
TriPTIT2003 <minhtri04062003@gmail.com>
2025-11-14 07:06:16 +0700

    Issue backend  :11

--- server/routes/routers/transactionRouter.js
+++ server/routes/routers/transactionRouter.js
@@ -2,9 +2,10 @@
 import express from "express";
 import { createCheckoutSession } from "../../controller/transactionController.js";
 import { isAuth } from "../../middleware/auth.middleware.js";
+import Client from "../../model/clientModel.js";
 
 const router = express.Router();
 
-router.route("/checkout-session").post(isAuth, createCheckoutSession);
+router.route("/checkout-session").post(isAuth(Client), createCheckoutSession);
 
 export default router;
